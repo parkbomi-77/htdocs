a0e8b9b6fa687d4b747e9c76d9aaaf87911394ea
parkbomi-77 <vnstn785@naver.com>
2022-07-08 19:07:14 +0900

    업데이트 안되는 에러 수정

--- wp-admin/edit-form-playtime.php
+++ wp-admin/edit-form-playtime.php
@@ -41,70 +41,66 @@
 
 <?php
 
-global $wpdb, $post;
-$results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_play_time where posts_lesson_id = $post->ID"));
+    global $wpdb, $post;
+    $results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_play_time where posts_lesson_id = $post->ID"));
 
-var_dump($results[0]->play_idx);
-?>
-<!-- 새로 등록할시 !  -->
-<?php 
-    $num = count($results);
-if(!$results){?> 
-<div class="playbox-container">
-    <p>vimeo 영상시간 : 상품등록</p>
-    <div class="playbox-list">
-        <div class="playbox">
-            <div class="playbox-num">1</div>
-            <input type="hidden" name="playboxNum[]" value="1">
-            <div class="playbox-time">
-                <input type="text" id="" name="playtime[]" value="<?php echo esc_attr( $post->playtime ); ?>" placeholder="00:00">
+
+    // 새로 등록할시 !  
+
+        $num = count($results);
+    if(!$results){
+?> 
+    <div class="playbox-container">
+        <p>vimeo 영상시간 : 상품등록</p>
+        <div class="playbox-list">
+            <div class="playbox">
+                <div class="playbox-num">1</div>
+                <input type="hidden" name="playboxNum[]" value="1">
+                <div class="playbox-time">
+                    <input type="text" id="" name="playtime[]" value="<?php echo esc_attr( $post->playtime ); ?>" placeholder="00:00">
+                </div>
+                <div class="playbox-name">
+                    <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value="<?php echo esc_attr( $post->playname ); ?>">
+                </div>
+                <div class="playbox-trash" onclick="close_boxTag()">✖︎</div>
             </div>
-            <div class="playbox-name">
-                <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value="<?php echo esc_attr( $post->playname ); ?>">
-            </div>
-            <div class="playbox-trash" onclick="close_boxTag()">✖︎</div>
+        </div>
+        <div class="playbox-add" onclick="create_boxTag()">
+            <div>+</div>
+            <div>신규</div>
         </div>
     </div>
-    <div class="playbox-add" onclick="create_boxTag()">
-        <div>+</div>
-        <div>신규</div>
-    </div>
-</div>
-<!-- 기존 설정 수정할시 -->
-<?php } else { 
+    <!-- 기존 설정 수정할시 화면에 띄우기 -->
+    <?php } else { 
     // $num = count($results);
-    $play_box = '';
-    for($i = 0; $i < $num; $i++){
-        $add_play_box = ' <div class="playbox">
-                            <div class="playbox-num">'.$results[$i]->play_idx.'</div>
-                            <input type="hidden" name="playboxNum[]" value="1">
-                            <div class="playbox-time">
-                                <input type="text" id="" name="playtime[]" value="'.$results[$i]->product_time.'" placeholder="00:00">
-                            </div>
-                            <div class="playbox-name">
-                                <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value=" '.$results[$i]->product_name.'">
-                            </div>
-                            <div class="playbox-trash" onclick="close_boxTag()">✖︎</div>
-                        </div>';
-        $play_box = $play_box.$add_play_box ;
+        $play_box = '';
+        for($i = 0; $i < $num; $i++){
+            $add_play_box = ' <div class="playbox">
+                                <div class="playbox-num">'.$results[$i]->play_idx.'</div>
+                                <input type="hidden" name="playboxNum[]" value="'.$results[$i]->play_idx.'">
+                                <div class="playbox-time">
+                                    <input type="text" id="" name="playtime[]" value="'.$results[$i]->product_time.'" placeholder="00:00">
+                                </div>
+                                <div class="playbox-name">
+                                    <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value=" '.$results[$i]->product_name.'">
+                                </div>
+                                <div class="playbox-trash" onclick="close_boxTag()">✖︎</div>
+                            </div>';
+            $play_box = $play_box.$add_play_box ;
+        }
+            echo ('<div class="playbox-container">
+                        <p>vimeo 영상시간 : 상품등록</p>
+                        <div class="playbox-list">
+                            '.$play_box.'
+                        </div>
+                        <div class="playbox-add" onclick="create_boxTag()">
+                            <div>+</div>
+                            <div>신규</div>
+                        </div>
+                    </div>');
+
     }
-        echo ('<div class="playbox-container">
-                    <p>vimeo 영상시간 : 상품등록</p>
-                    <div class="playbox-list">
-                        '.$play_box.'
-                    </div>
-                    <div class="playbox-add" onclick="create_boxTag()">
-                        <div>+</div>
-                        <div>신규</div>
-                    </div>
-                </div>');
-
-}
-
-
-
-
-?>
+    ?>
 
 
 
