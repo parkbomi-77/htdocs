71ceeaad7858a527186b6da62f049aa21cdbfb43
parkbomi-77 <vnstn785@naver.com>
2022-07-11 10:25:36 +0900

    콜론형식으로 재생시간 입력받기 구현

--- wp-admin/edit-form-playtime.php
+++ wp-admin/edit-form-playtime.php
@@ -26,6 +26,9 @@
 .playbox-name input{
     width: 100%;
 }
+.playbox-trash{
+    font: 30px;
+}
 /* 신규 추가 버튼 */
 .playbox-add{
     display: flex;
@@ -42,12 +45,11 @@
 <?php
 
     global $wpdb, $post;
+    // lesson 글에 해당하는 재생시간등록 결과 불러오기 
     $results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_play_time where posts_lesson_id = $post->ID"));
-
-
+    $num = count($results);
+    
     // 새로 등록할시 !  
-
-        $num = count($results);
     if(!$results){
 ?> 
     <div class="playbox-container">
@@ -57,7 +59,9 @@
                 <div class="playbox-num">1</div>
                 <input type="hidden" name="playboxNum[]" value="1">
                 <div class="playbox-time">
-                    <input type="text" id="" name="playtime[]" value="<?php echo esc_attr( $post->playtime ); ?>" placeholder="00:00">
+                    <input type="text" id="" name="playtime[]" 
+                    value="<?php echo esc_attr( $post->playtime ); ?>" placeholder="00:00" maxlength="5"
+                    onKeyup="inputTimeColon(this)">
                 </div>
                 <div class="playbox-name">
                     <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value="<?php echo esc_attr( $post->playname ); ?>">
@@ -70,16 +74,17 @@
             <div>신규</div>
         </div>
     </div>
+    <?php $num++; ?>
     <!-- 기존 설정 수정할시 화면에 띄우기 -->
     <?php } else { 
-    // $num = count($results);
+    // play_box를 붙히는 형식 
         $play_box = '';
         for($i = 0; $i < $num; $i++){
             $add_play_box = ' <div class="playbox">
                                 <div class="playbox-num">'.$results[$i]->play_idx.'</div>
                                 <input type="hidden" name="playboxNum[]" value="'.$results[$i]->play_idx.'">
                                 <div class="playbox-time">
-                                    <input type="text" id="" name="playtime[]" value="'.$results[$i]->product_time.'" placeholder="00:00">
+                                    <input type="text" id="" name="playtime[]" value="'.$results[$i]->product_time.'" maxlength="5" onKeyup="inputTimeColon(this)">
                                 </div>
                                 <div class="playbox-name">
                                     <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value=" '.$results[$i]->product_name.'">
@@ -119,7 +124,7 @@
                 `<div class="playbox-num">${Count}</div>
                 <input type="hidden" name="playboxNum[]" value=${Count}>
                 <div class="playbox-time">
-                    <input type="text" id="" name="playtime[]" value="<?php echo esc_attr( $post->playtime ); ?>" placeholder="00:00">
+                    <input type="text" id="" name="playtime[]" value="<?php echo esc_attr( $post->playtime ); ?>" maxlength="5" onKeyup="inputTimeColon(this)">
                 </div>
                 <div class="playbox-name">
                     <input type="text" id="" name="playname[]" placeholder="제품명 입력란(40)" value="<?php echo esc_attr( $post->playname ); ?>">
@@ -136,6 +141,27 @@
         let deletebox = document.querySelector('.playbox-list').lastChild;
         playboxList.removeChild(deletebox);
         Count = Count-1;
+    }
+
+
+    // 재생시간 콜론(:) 으로 입력받는 함수 
+    function inputTimeColon(time) {
+        let replaceTime = time.value.replace(/\:/g, "");
+
+        let minute = replaceTime.substring(0, 2);      // 선언한 변수 hours에 시간값을 담는다.
+        let seconds = replaceTime.substring(2, 4);    // 선언한 변수 minute에 분을 담는다.
+
+        if(isFinite(minute + seconds) == false) {
+            alert("문자는 입력하실 수 없습니다.");
+            time.value = "00:00";
+            return false;
+        }
+
+        if(seconds > 59 ) {
+                alert("초는 1분단위 아래로 입력해주세요.");
+                return false;
+        }
+        time.value = minute + ":" + seconds;
     }
 
 </script>
