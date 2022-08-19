2be2be23c8c734d43fe5ed128d964a1d82099536
parkbomi-77 <vnstn785@naver.com>
2022-08-19 11:04:06 +0900

    벳스쿨에서 광고제품 삭제할때 그누보드 shop_item DB 광고여부에 0 표시

--- practice/gnuboard/product_list.php
+++ practice/gnuboard/product_list.php
@@ -2,19 +2,31 @@
 <?php
 include_once('./_common.php');
 
-$_POST['product_code'];
+if($_POST['product_code']){ //벳스쿨에 광고제품 등록했을때
+    // 벳스쿨에서 등록한 제품ID가 그누보드쪽 제품ID와 일치하는지 체크
+    $sql = "SELECT * FROM {$g5['g5_shop_item_table']} where it_id = {$_POST['product_code']}";
+    $item = sql_query($sql);
+    
+    if($item){ // 일치하면 it_1_subj( 광고여부필드 ) 1로 업데이트 
+        $sql2 = "UPDATE {$g5['g5_shop_item_table']} SET `it_1_subj` = '1' where it_id = {$_POST['product_code']}";
+        sql_query($sql2);
+    
+    }else { // 없으면? 잘못들어온 데이터 
+        return;
+    }
+}else if($_POST['delete_code']){ // 벳스쿨에 광고제품 삭제했을때 
+    // 벳스쿨에서 삭제 제품ID가 그누보드쪽 제품인지 체크
+    $sql = "SELECT * FROM {$g5['g5_shop_item_table']} where it_id = {$_POST['delete_code']}";
+    $item = sql_query($sql);
 
-
-// 벳스쿨에서 등록한 제품ID가 그누보드쪽 제품ID와 일치하는지 체크
-$sql = "SELECT * FROM {$g5['g5_shop_item_table']} where it_id = {$_POST['product_code']}";
-$item = sql_query($sql);
-
-if($item){ // 일치하면 it_1_subj( 광고여부필드 ) 1로 업데이트 
-    $sql2 = "UPDATE {$g5['g5_shop_item_table']} SET `it_1_subj` = '1' where it_id = {$_POST['product_code']}";
-    sql_query($sql2);
-
-}else { // 없으면? 잘못들어온 데이터 
-    return;
+    if($item){ // 맞으면 it_1_subj( 광고여부필드 ) 0으로 변경
+        $sql2 = "UPDATE {$g5['g5_shop_item_table']} SET `it_1_subj` = '0' where it_id = {$_POST['delete_code']}";
+        sql_query($sql2);
+    }else { // 없으면? 잘못들어온 데이터 
+        return;
+    }
 }
+
+
 
 ?>
--- wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-delete.php
+++ wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-delete.php
@@ -6,6 +6,25 @@ define( 'SHORTINIT', true );
 require_once( $_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
 
 for($i=0; $i<$num; $i++){
+    $results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_product_list where ID={$deletecheck[$i]}"));
+    $results2 = $results[0] -> product_code;
+    $postdata = http_build_query(
+        array(
+            'delete_code' => $results2
+        )
+    );
+    $opts = array('http' =>
+        array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+    $context = stream_context_create($opts);
+    file_get_contents('http://localhost:8888/practice/gnuboard/product_list.php', false, $context);
+
+
+    // 벳스쿨 광고상품 DB에서 삭제 
     $wpdb->delete('wp_product_list', 
     array(
         'ID' => $deletecheck[$i]
