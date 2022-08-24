43333f1385d1b85cbb4ea858e6e388d47e272b49
parkbomi-77 <vnstn785@naver.com>
2022-08-24 18:53:48 +0900

    광고의뢰한 쇼핑몰 리스트 관리 탭 - 수정,삭제 기능

--- wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php
+++ wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php
@@ -6,20 +6,44 @@ require_once( $_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
 
 
 $mall = $_POST['name'];
-// 광고의뢰 쇼핑몰 리스트 빈 값은 걸러내기
-function empty_ ($var) {
-    if($var !== ""){
-        return $var;
+$link = $_POST['link'];
+
+$delcode = $_POST['code'];
+$editcode = $_POST['editcode'];
+$newcode = $_POST['newcode'];
+$newname = $_POST['newname'];
+$newlink = $_POST['newlink'];
+
+
+
+if($delcode){ // 삭제 요청
+    $wpdb->get_results($wpdb->prepare("DELETE FROM wp_shoppingmall where (code ='".$delcode."')"));
+}else if($editcode){ // 수정 사항 보여주기 요청
+    $results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_shoppingmall where code ='".$editcode."' "));
+    // echo $results['code'];
+    // echo $results['name'];
+    // echo $results['link'];
+    $dataArray[0] = '삽입 성공';
+    echo json_encode($dataArray);
+}else if($newcode){ // 수정사항 저장하기 
+    $zzx = "UPDATE wp_shoppingmall set name= '".$newname."', link='".$newlink."' where code ='".$newcode."' ";
+    $wpdb->get_results($wpdb->prepare("UPDATE wp_shoppingmall set name= '".$newname."', link='".$newlink."' where code ='".$newcode."' "));
+}else {
+    // 광고의뢰 쇼핑몰 리스트 빈 값은 걸러내기
+    function empty_ ($var) {
+        if($var !== ""){
+            return $var;
+        }
     }
-}
-$newmall = array_filter($mall, "empty_");
-
-$results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_shoppingmall"));
-$num = count($results)-1;
-$nodenum = $results[$num]->code;
-
-for($i=0; $i<count($newmall); $i++){
-    $wpdb->get_results($wpdb->prepare("INSERT INTO wp_shoppingmall (`name`) VALUES ('".$newmall[$i]."');"));
+    $newmall = array_filter($mall, "empty_");
+    
+    $results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_shoppingmall"));
+    $num = count($results)-1;
+    $nodenum = $results[$num]->code;
+    
+    for($i=0; $i<count($newmall); $i++){
+        $ppp = $wpdb->get_results($wpdb->prepare("INSERT INTO wp_shoppingmall (name,link) VALUES ('".$newmall[$i]."','".$link[$i]."');"));
+    }
 }
 
 
@@ -27,4 +51,5 @@ $prevPage = $_SERVER['HTTP_REFERER'];
 // 변수에 이전페이지 정보를 저장
 $location ='location:'.$prevPage.'#shopping_mall';
 header($location);
+
 ?>
--- wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall.php
+++ wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall.php
@@ -5,8 +5,15 @@
     if($results){ // 벳스쿨쪽으로 광고의뢰한 쇼핑몰이 있을 경우 
         $mall_lists = "";
         for($i=0; $i<count($results); $i++){
-            $mall_list = '<tr><td>'.($i+1).'</td>
-            <td>'.$results[$i]->name.'</td></tr>';
+            $mall_list = '<tr id="shoppingmall-row">
+            <td>'.($i+1).'</td>
+            <td>'.$results[$i]->name.'</td>
+            <td>'.$results[$i]->link.'</td>
+            <td id="shoppingmall-box-btn">
+                <button class="shoppingmall-box-edit" onclick="edit(this)" value="'.$results[$i]->name.'"><i class="fas fa-edit"></i></button>
+                <button class="shoppingmall-box-del" onclick="del(this)" value="'.$results[$i]->code.'"><i class="fas fa-times-circle"></i></button>
+            </td>
+            </tr>';
             $mall_lists = $mall_lists.$mall_list;
         }
     }else { // 벳스쿨쪽으로 광고의뢰한 쇼핑몰이 없을 경우 
@@ -25,18 +32,24 @@
     <div class="shoppingmall-box">
         <table style="text-align:center;" class="shoppingmalltable">
             <colgroup>
-                <col width="20%">
-                <col width="80%">
+                <col width="10%">
+                <col width="30%">
+                <col width="47%">
+                <col width="13%">
             </colgroup>
             <thead>
                 <tr> 
                     <th>no.</th>
-                    <th>shoppiing mall list</th>
+                    <th>name</th>
+                    <th>link</th>
+                    <th></th>
                 </tr>
             </thead>
-            <tbody  id="name">
+            <form action="http://localhost:8888/wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php" method="post">
+                <tbody id="name">
                     <?php echo $mall_lists ?>
-            </tbody>
+                </tbody>
+            </form>
         </table>
         <div class="edit-btn" onclick="add()">add</div>
     </div>
@@ -46,7 +59,9 @@
         <form action="http://localhost:8888/wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php" method="post">
             <div class="shoppingmall-box2-header">
                 <div class="shoppingmall-box2-num">no.</div>
-                <div class="shoppingmall-box2-name">shoppiing mall list</div>
+                <div class="shoppingmall-box2-name">name</div>
+                <div class="shoppingmall-box2-link">link</div>
+
             </div>
             <div class="shoppingmall-box2-body">
                 <?php
@@ -54,18 +69,16 @@
                     echo '<div class="shoppingmall-box2-row">
                             <div class="shoppingmall-box2-num">'.($i+1).'</div>
                             <div class="shoppingmall-box2-name"><input type="text" name="name[]"></div>
+                            <div class="shoppingmall-box2-link"><input type="text" name="link[]"></div>
                         </div>';
                 }
                 ?>
-                <!-- <div>
-                    <div class="shoppingmall-box2-num">0</div>
-                    <div class="shoppingmall-box2-name"><input type="text"></div>
-                </div> -->
                 <input type="hidden" value="">
             </div>
             
             
             <div class="shoppingmall-box2-add" onclick="inadd()">add</div>
+            <div class="back-btn" onclick="back()">back</div>
             <div class="edit-btn" ><input type="submit" id="" value="save"></div>
         </form>
     </div>
@@ -75,8 +88,9 @@
 
 <script src="//code.jquery.com/jquery.min.js"></script>
 <script>
+    let state = ''
+
     function add(){
-        // let tbody = document.querySelector("#name")
         document.querySelector(".shoppingmall-box").classList.add('none');
         document.querySelector(".shoppingmall-box2").classList.remove('none');
     }
@@ -88,11 +102,98 @@
             addChild = addChild + `<div>
                 <div class="shoppingmall-box2-num">${i}</div>
                 <div class="shoppingmall-box2-name"><input type="text"></div>
+                <div class="shoppingmall-box2-link"><input type="text"></div>
             </div>`
         }
         // document.parent.appendChild(addChild);
         $(parent).append(addChild);
     }
+    function edit(e) {
+        if(state === ''){
+            let trtag = e.parentElement.parentElement;
+            let name = trtag.children[1];
+            let link = trtag.children[2];
+            let btn = trtag.children[3];
+            let code = btn.children[1].value;
+            console.log(btn.children[1].value)
+      
+            name.innerHTML = `<input type="text" style="width:98%" name="name" value="${e.value}">`
+            link.innerHTML = `<input type="text" style="width:100%" name="link" value="${link.innerHTML}">`
+            btn.innerHTML = 
+            `<button type="submit" class="shoppingmall-edit-confirm" onclick="editsave(this)" value="${code}"><i class="fas fa-check"></i></button>
+            <button type="submit" class="shoppingmall-edit-confirm" onclick="reset(this)" value="${code}"><i class="fas fa-redo"></i></i></button>`
+            state = e.value;
+        }else {
+            alert("1개씩 수정해주세요")
+        }
+    }
+    function del(e) {
+        if(window.confirm("정말 삭제하시겠습니까?")){
+            $.ajax({
+                url: "http://localhost:8888/wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php",
+                type: "post",
+                dataType : 'json',
+                data: {
+                    code : e.value,
+                },
+            })
+            window.location.reload();
+        } else {
+            return;
+        }
+    }
+    function editsave(e) {
+        let trtag = e.parentElement.parentElement;
+        let name = trtag.children[1];
+        let new_name = name.children[0].value;
+        let link = trtag.children[2];
+        let new_link = link.children[0].value;
+        let newcode = e.value;
+        console.log(newcode);
+
+        if(window.confirm("수정하시겠습니까?")){
+            $.ajax({
+                url: "http://localhost:8888/wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php",
+                type: "post",
+                dataType : 'json',
+                data: {
+                    newcode,
+                    newname : new_name,
+                    newlink : new_link
+                },
+            })
+            state = "";
+            window.location.reload();
+        } else {
+            return;
+        }
+
+    }
+    function reset(e) {
+        let trtag = e.parentElement.parentElement;
+        let num = trtag.children[0];
+        let reset_num = num.innerHTML
+        let name = trtag.children[1];
+        let reset_name = name.children[0].value;
+        let link = trtag.children[2];
+        let reset_link = link.children[0].value;
+        let btn = trtag.children[3];
+        let code = e.value;
+
+        trtag.innerHTML = `<td>${reset_num}</td>
+            <td>${reset_name}</td>
+            <td>${reset_link}</td>
+            <td id="shoppingmall-box-btn">
+                <button class="shoppingmall-box-edit" onclick="edit(this)" value="${reset_name}"><i class="fas fa-edit"></i></button>
+                <button class="shoppingmall-box-del" onclick="del(this)" value="${code}"><i class="fas fa-times-circle"></i></button>
+            </td>`;
+        state = "";
+    }
+    function back() {
+        document.querySelector(".shoppingmall-box").classList.remove('none');
+        document.querySelector(".shoppingmall-box2").classList.add('none');
+    }
 
 </script>
+
 
