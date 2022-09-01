7e7c552caa8d330e8b339bbf1f4d72976607b993
parkbomi-77 <vnstn785@naver.com>
2022-09-01 16:43:02 +0900

    벳스쿨 등록한 쇼핑몰 삭제시 광고제품,영상에 등록한 제품, 장바구니 데이터 안보이게하기

--- wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php
+++ wp-content/plugins/masterstudy-lms-learning-management-system/nuxy/metaboxes/metabox-shoppingmall-save.php
@@ -7,6 +7,8 @@ require_once( $_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
 
 $mall = $_POST['name'];
 $link = $_POST['link'];
+$link2 = $_POST['link2'];
+
 
 $delcode = $_POST['code'];
 $editcode = $_POST['editcode'];
@@ -19,13 +21,34 @@ $newlink2 = $_POST['newlink2'];
 
 
 if($delcode){ // 삭제 요청
-    $wpdb->get_results($wpdb->prepare("DELETE FROM wp_shoppingmall where (code ='".$delcode."')"));
+    // 쇼핑몰리스트 활성화0 으로
+    $wpdb->get_results($wpdb->prepare("UPDATE wp_shoppingmall set state=0 where (code ='".$delcode."')"));
+
+    $result = $wpdb->get_results($wpdb->prepare("SELECT * FROM wp_product_list where (mall_code ='".$delcode."')"));
+    for($i=0; $i<count($result); $i++){
+        // 광고제품 리스트 광고활성화 0으로
+        $wpdb->get_results($wpdb->prepare("UPDATE wp_product_list set adv_state=0 where (ID ='".$result[$i] -> ID."')"));
+        // 영상에 등록되어져있는 제품들 삭제
+        $result2 = $wpdb->get_results($wpdb->prepare("SELECT * FROM wp_play_time where product_list_id =".$result[$i] -> ID));
+        for($j=0; $j<count($result2); $j++){
+            $wpdb->get_results($wpdb->prepare("DELETE FROM wp_play_time where (ID ='".$result2[$j]->ID."')"));
+        }
+        // 장바구니에 있는제품들 삭제
+        $result3 = $wpdb->get_results($wpdb->prepare("SELECT * FROM wp_wish_list where item_id =".$result[$i]->ID));
+        for($k=0; $k<count($result3); $k++){
+            $wpdb->get_results($wpdb->prepare("DELETE FROM wp_wish_list where (ID ='".$result3[$k]->ID."')"));
+        }
+        
+
+
+    }
+
+
 }else if($editcode){ // 수정 사항 보여주기 요청
     $results = $wpdb->get_results($wpdb->prepare("SELECT * from wp_shoppingmall where code ='".$editcode."' "));
     $dataArray[0] = '삽입 성공';
     echo json_encode($dataArray);
 }else if($newcode){ // 수정사항 저장하기 
-    $zzx = "UPDATE wp_shoppingmall set name= '".$newname."', link='".$newlink."' where code ='".$newcode."' ";
     $wpdb->get_results($wpdb->prepare("UPDATE wp_shoppingmall set name= '".$newname."', link='".$newlink."', link2='".$newlink2."' where code ='".$newcode."' "));
 }else {
     // 광고의뢰 쇼핑몰 리스트 빈 값은 걸러내기
@@ -41,7 +64,7 @@ if($delcode){ // 삭제 요청
     $nodenum = $results[$num]->code;
     
     for($i=0; $i<count($newmall); $i++){
-        $ppp = $wpdb->get_results($wpdb->prepare("INSERT INTO wp_shoppingmall (name,link) VALUES ('".$newmall[$i]."','".$link[$i]."');"));
+        $ppp = $wpdb->get_results($wpdb->prepare("INSERT INTO wp_shoppingmall (name,link, link2, state) VALUES ('{$newmall[$i]}','{$link[$i]}','{$link2[$i]}',1);"));
     }
 }
 
