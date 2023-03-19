73e760b160055114fd7c812573c6de2135fbc66b
parkbomi-77 <vnstn785@naver.com>
2023-03-19 14:47:42 +0900

    벳스쿨 비회원이 구매시도시 로그인or회원가입 페이지로 이동

--- wp-content/plugins/woocommerce/templates/checkout/form-login.php
+++ wp-content/plugins/woocommerce/templates/checkout/form-login.php
@@ -23,14 +23,15 @@ if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_lo
 
 ?>
 <div class="woocommerce-form-login-toggle">
-	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_login_message', esc_html__( 'Returning customer?', 'woocommerce' ) ) . ' <a href="#" class="showlogin">' . esc_html__( 'Click here to login', 'woocommerce' ) . '</a>', 'notice' ); ?>
+	<?php wc_print_notice( '<br>'.apply_filters( 'woocommerce_checkout_login_message', esc_html__( 'Returning customer?', 'woocommerce' ) ) . ' <a href="#" class="showlogin">' . esc_html__( 'Click here to login', 'woocommerce' ) . '</a>'.'<br> 신규고객이신 분은 <a href="/register" class="">' . '회원가입' . '</a>하신 후 이용해주세요', 'notice' ); ?>
+
 </div>
 <?php
 
 woocommerce_login_form(
 	array(
-		'message'  => esc_html__( 'If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing section.', 'woocommerce' ),
-		'redirect' => wc_get_checkout_url(),
+		//'message'  => esc_html__( 'If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing section.', 'woocommerce' ),
+		'redirect' => wc_get_cart_url(),
 		'hidden'   => true,
 	)
 );
