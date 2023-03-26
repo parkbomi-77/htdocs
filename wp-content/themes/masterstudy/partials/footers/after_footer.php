df8acc5ca5cf34338487176de97335b0fecb5894
parkbomi-77 <vnstn785@naver.com>
2023-03-27 00:13:38 +0900

    벳스쿨 몸무게 2-30키로 입력 제한

--- wp-content/themes/masterstudy/partials/footers/after_footer.php
+++ wp-content/themes/masterstudy/partials/footers/after_footer.php
@@ -11,6 +11,25 @@
         var hoursStr = '<?php esc_html_e('Hours', 'masterstudy'); ?>';
         var minutesStr = '<?php esc_html_e('Minutes', 'masterstudy'); ?>';
         var secondsStr = '<?php esc_html_e('Seconds', 'masterstudy'); ?>';
+
+        // 몸무게 입력받는 input 태그
+        let billingWeight = document.getElementById("billing_weight");
+  
+        // billingWeight.setAttribute("min", "2");
+        // billingWeight.setAttribute("max", "30");
+        billingWeight.value = 2;
+
+        billingWeight.addEventListener("change", function() {
+            let nowValue = billingWeight.value;
+            if(nowValue < 2) {
+                alert("2kg이상부터 사용이 가능합니다.")
+                billingWeight.value = 2
+            }else if(nowValue >30) {
+                alert("30kg 이하로 사용이 가능합니다. ")
+                billingWeight.value = 30;
+            }
+        })
+
     </script>
 
 <?php
